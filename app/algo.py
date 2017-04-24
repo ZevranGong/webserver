817ed92b42fd68e6922ec2cd8c87efc15056a438
Zevran <ziyuangong@gmail.com>
2017-04-24 03:01:05 -0400

    message

---
+++ app/algo.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+def algo(data):
+	for i in range(2**len(data)):
+		continue
+	return len(data)
---
+++ app/views.py
@@ -0,0 +1,30 @@
+from flask import render_template, flash, redirect,make_response
+from app import app
+from .forms import RequestForm
+from .algo import algo
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Miguel'}
+    post = {'prompt': 'Joy is the birth of interconnectedness, and of us. \
+    Consciousness consists of chaos-driven reactions of quantum energy. \
+    “Quantum” means a deepening of the Vedic. We live, we live, we are reborn.\
+    Without serenity, one cannot reflect.\
+    Greed is born in the gap where interconnectedness has been excluded. \
+    Discontinuity is the antithesis of insight.'}
+    return render_template("index.html",
+                           title='Home',
+                           post = post)
+
+
+@app.route('/request', methods = ['GET', 'POST'])
+def request():
+    form = RequestForm()
+    if form.validate_on_submit():
+        flash('Your request of "%s" has been submitted' % (form.req.data))
+        #return render_template("index.html",title='Home',post = str(algo(form.req.data)))
+        return make_response(str(algo(form.req.data)))
+    return render_template('forms.html', 
+        title = 'request',
+        form = form)
