fba9276ea2dbc42c7a83e1a345c73714daed8a43
Zevran <ziyuang@andrew.cmu.edu>
2017-04-24 13:40:54 -0400

    make request the homepage

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from .algo import algo
 @app.route('/')
 @app.route('/index')
 def index():
+    # the following strings are for testing purposes
     user = {'nickname': 'Miguel'}
     post = {'prompt': 'Joy is the birth of interconnectedness, and of us. \
     Consciousness consists of chaos-driven reactions of quantum energy. \
@@ -13,10 +14,7 @@ def index():
     Without serenity, one cannot reflect.\
     Greed is born in the gap where interconnectedness has been excluded. \
     Discontinuity is the antithesis of insight.'}
-    return render_template("index.html",
-                           title='Home',
-                           post = post)
-
+    return redirect('/request')
 
 @app.route('/request', methods = ['GET', 'POST'])
 def request():
